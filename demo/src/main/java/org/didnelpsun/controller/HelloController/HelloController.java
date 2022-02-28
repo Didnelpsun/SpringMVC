9f70bded73e23f50a28a0b0eae8e4ae322a59ef5
Didnelpsun <2675350965@qq.com>
2022-02-28 23:10:14 +0800

    更新

---
+++ demo/src/main/java/org/didnelpsun/controller/HelloController/HelloController.java
@@ -0,0 +1,10 @@
+// HelloController.java
+package org.didnelpsun.controller.HelloController;
+
+public class HelloController {
+    public String sayHello(){
+        String string = "hello springmvc";
+        System.out.println(string);
+        return string;
+    }
+}
